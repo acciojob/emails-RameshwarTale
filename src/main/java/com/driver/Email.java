7bd2613e9b5491bd4f71cda98a30cc0322267899
Rameshwar Tale <ramgtale21@gmail.com>
2024-02-26 15:32:49 +0530

    ans

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,38 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+
+        int size = newPassword.length();
+        if(this.password.equals(oldPassword) && size>=8){
+
+             boolean upperCase = false;
+             boolean lowerCase = false;
+             boolean digit =  false;
+             boolean specialChar = false;
+
+             for(int i=0; i<size; i++){
+                 char ch = newPassword.charAt(i);
+
+                 if(ch>='A' && ch<='z'){
+                     upperCase = true;
+                 }
+                 else if(ch>='a' && ch<='z'){
+                     lowerCase = true;
+                 }
+                 else if(ch>='0' && ch<='9'){
+                     digit = true;
+                 }
+                 else{
+                     specialChar = true;
+                 }
+             }
+
+            if(upperCase && lowerCase && digit && specialChar){
+                this.password = newPassword;
+            }
+        }
+        else{
+            return;
+        }
     }
 }
